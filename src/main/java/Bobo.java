e4410e7a39a5eac3ef5f708b4818f4422a7c543e
Krithic <krithic04@gmail.com>
2026-08-21 15:13:02 +0800

    Bot now repeats what I say and exits when I say bye

--- src/main/java/Bobo.java
+++ src/main/java/Bobo.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Bobo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+
         String banner = " ____        _                \n" +
         "| __ )  ___ | |__   ___   \n" +
         "|  _ \\ / _ \\| '_ \\ / _ \\  \n" +
         "| |_) | (_) | |_) | (_) | \n" +
         "|____/ \\___/|_.__/ \\___/  \n";
         System.out.println(banner);
-        System.out.println("Hello! I'm Bobo.\nWhat can I do for you?\n____________________________________________________________\nBye. Hope to see you again soon!\n____________________________________________________________");
-    }
+        System.out.println("Hello! I'm Bobo.\nWhat can I do for you?");
+        String text = scanner.nextLine(); 
+        
+        while (!text.equals("bye")) {   
+            System.out.println(text);
+            text = scanner.nextLine(); 
+        }
+        
+        // 4. Close the scanner to prevent memory leaks
+        scanner.close();
+        System.out.println("\n____________________________________________________________\nBye. Hope to see you again soon!\n____________________________________________________________");
+
+        }
+        
 }
