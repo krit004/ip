471e9497c51fbb6a475537c6065a51256069916c
Krithic <krithic04@gmail.com>
2026-08-21 14:58:31 +0800

    Renamed chatbot to Bobo

---
+++ src/main/java/Bobo.java
@@ -0,0 +1,11 @@
+public class Bobo {
+    public static void main(String[] args) {
+        String banner = " ____        _                \n" +
+        "| __ )  ___ | |__   ___   \n" +
+        "|  _ \\ / _ \\| '_ \\ / _ \\  \n" +
+        "| |_) | (_) | |_) | (_) | \n" +
+        "|____/ \\___/|_.__/ \\___/  \n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm Bobo.\nWhat can I do for you?\n____________________________________________________________\nBye. Hope to see you again soon!\n____________________________________________________________");
+    }
+}
