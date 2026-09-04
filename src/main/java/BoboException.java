79b64ab315f6d9d490e68f5c5a587bd0db7e9d13
Krithic <krithic04@gmail.com>
2026-09-04 13:10:46 +0800

    Edit to make code more OOP

---
+++ src/main/java/BoboException.java
@@ -0,0 +1,14 @@
+/**
+ * Represents exceptions specific to the Bobo chatbot application.
+ */
+public class BoboException extends Exception {
+
+    /**
+     * Constructs a BoboException with the specified error message.
+     *
+     * @param message The detailed error message.
+     */
+    public BoboException(String message) {
+        super(message);
+    }
+}
